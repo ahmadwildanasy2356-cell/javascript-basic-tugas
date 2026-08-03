925636e9bafce682eabdef333cd4a7187668c3a5
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-03 15:07:34 +0700

    bonus

---
+++ bab-07-perulangan/latihan/bonus.js
@@ -0,0 +1,14 @@
+const pelanggan = [
+  "Andi",
+  "Budi",
+  "Citra",
+  "Dewi",
+  "Eko",
+];
+
+for (let i = 0; i < pelanggan.length; i++) {
+  console.log("Memanggil pelanggan :", pelanggan[i]);
+}
+  console.log("\nSemua pelanggan telah di layani.")
+
+
