cda93f2ed688a027cc37a9b21dbb2ba4b3b3ff93
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-04 13:52:14 +0700

    Belanja

---
+++ bab-08-array-method/latihan/soal13.js
@@ -0,0 +1,12 @@
+let belanja = [];
+
+belanja.push("Beras");
+belanja.push("Minyak");
+belanja.push("Gula");
+
+belanja.pop();
+
+belanja.push("Telur");
+belanja.unshift("Teping");
+ console.log("isi :" ,belanja)
+ console.log("Jumlah Belanja=", belanja.length)
