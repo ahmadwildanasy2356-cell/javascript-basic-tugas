dd56959e9e739b5d04b372fd1928a76d9f1e8852
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-04 13:19:16 +0700

    indexOf

---
+++ bab-08-array-method/latihan/soal08.js
@@ -0,0 +1,12 @@
+
+let kota = [
+  "Jakarta",
+  "Bandung",
+  "Yogyakarta",
+  "Surabaya",
+];
+
+
+console.log(kota.indexOf("Bandung")); 
+console.log(kota.indexOf("Surabaya")); 
+console.log(kota.indexOf("Bali")); 
