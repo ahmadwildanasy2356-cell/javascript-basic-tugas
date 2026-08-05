21db7987ef03090dab1617b21be34fba13945ea8
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-05 14:58:33 +0700

    soal12

---
+++ bab-09-fungsi/latihan/soal11.js
@@ -0,0 +1,9 @@
+function biodata(nama, umur, pekerjaan) {
+  console.log("========== BIODATA =========");
+  console.log("Nama      :", nama);
+  console.log("Umur      :", umur ,"tahun");
+  console.log("Pekerjaan :", pekerjaan);
+  console.log("============================");
+}
+
+biodata("Ahmad", 25, "Doctor");
---
+++ bab-09-fungsi/latihan/soal12.js
@@ -0,0 +1,21 @@
+function tambah(a, b) {
+  return a + b;
+}
+
+function kurang(a, b) {
+  return a - b;
+}
+
+function kali(a, b) {
+  return a * b;
+}
+
+function bagi(a, b) {
+  return a / b;
+}
+
+
+console.log("Tambah :", tambah(10, 5));
+console.log("Kurang :", kurang(10, 5));
+console.log("Kali   :", kali(10, 5));
+console.log("Bagi   :", bagi(10, 5));
