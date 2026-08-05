8c9a26d2e10c072fa6831022e67b010472df0db8
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-05 10:36:55 +0700

    soal03

---
+++ bab-09-fungsi/latihan/soal03.js
@@ -0,0 +1,8 @@
+function tampilkanProduk(namaProduk, Harga, Stok) {
+  console.log("Produk :", namaProduk);
+  console.log("Harga  :", Harga);
+  console.log("Stok   :", Stok);
+  
+}
+
+tampilkanProduk("Mouse",15000, 12);
