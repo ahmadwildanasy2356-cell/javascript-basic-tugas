e8b6f19a98dbcc6eb1b17baf8d1ea3fc0608cd52
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-06 10:49:26 +0700

    soal03

---
+++ bab-10-object-method/latihan/soal03.js
@@ -0,0 +1,14 @@
+const mobil = {
+  merek: "Toyota",
+  warna: "Putih",
+};
+
+mobil.warna = "Hitam";
+
+
+mobil.tahun = 2025;
+
+
+console.log("merek :", mobil.merek);
+console.log("warna :", mobil.warna);
+console.log("tahun :", mobil.tahun);
