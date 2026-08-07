effca6add25068db106e1d3deaa6b4f9091aab7d
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-07 13:06:30 +0700

    soal01

---
+++ bab-11-iterable-dan-non-iterable/latihan/soal01.js
@@ -0,0 +1,5 @@
+"Indonesia" // iterable : karena string //
+["Merah", "Putih"] // iterable : karena array //
+150 // non iterable : karena number //
+true // non iterable : karena boolean //
+{ nama: "Budi" } // non iterable : karena object //
