ce72ca2ddd5f21e8d2ebaa3e158e844e31a12386
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-10 13:33:29 +0700

    soal06

---
+++ bab-12-string-number-built-in-function/latihan/soal06.js
@@ -0,0 +1,6 @@
+const url = "https://santri.dev";
+const posisi1 = url.startsWith("https");
+const posisi2 = url.endsWith(".dev");
+
+console.log("Mengggunakan HTTPS :", posisi1);
+console.log("Domain .dev :",  posisi2);
