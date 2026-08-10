c012c4c57eb951187056f66d3ebdfd52bce72fbd
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-10 14:16:05 +0700

    soal11

---
+++ bab-12-string-number-built-in-function/latihan/soal11.js
@@ -0,0 +1,11 @@
+const nilai1 = "85";
+const nilai2 = "90";
+
+console.log(nilai1 + nilai2);
+
+let angka1 = Number(nilai1);
+let angka2 = Number(nilai2);
+
+let total = angka1 + angka2
+
+console.log("Total nilai :", total);
