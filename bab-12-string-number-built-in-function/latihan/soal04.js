a01884ec0541ffd8957f71df26b2baa15154af8d
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-10 13:17:40 +0700

    soal04

---
+++ bab-12-string-number-built-in-function/latihan/soal04.js
@@ -0,0 +1,6 @@
+const pesan = "Selamat datang di kelas JavaScript!";
+if (pesan.includes("JavaScript")) {
+  console.log("Pesan membahas JavaScript");
+}else {
+    console.log("Pesan tidak membahas JavaScript");
+};
