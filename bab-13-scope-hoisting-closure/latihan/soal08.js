7cbde92e051ba72e27b730df76d0f0bfbaefa35b
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-11 14:41:26 +0700

    soal09

--- bab-13-scope-hoisting-closure/latihan/soal08.js
+++ bab-13-scope-hoisting-closure/latihan/soal08.js
@@ -2,4 +2,4 @@ console.log(nama);
 
 let nama = "Budi";
 
-// jawabannya : c. karena let melarang keras variabel dipanggil sebelum dibuat (isi / nilainya)
+// jawabannya : c. karena let melarang keras variabel dipanggil sebelum dibuat (isi / nilainya)/ dipanggil sebelum barisnya di baca 
---
+++ bab-13-scope-hoisting-closure/latihan/soal09.js
@@ -0,0 +1,11 @@
+console.log(umur);
+
+const umur = 17;
+
+console.log(umur);
+
+var umur = 17;
+
+// perbedaannya di output
+// kalo yang pakai const variabelnya tidak boleh dipanggil sebelum barisnya di baca
+// kalo pakai var boleh tapi hasilnya undefined
