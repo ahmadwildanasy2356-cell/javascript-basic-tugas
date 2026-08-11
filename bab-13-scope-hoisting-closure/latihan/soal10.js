d8dda5bfc6dd79078f2c74b2e330a40da8fab35d
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-11 15:04:29 +0700

    soal10

---
+++ bab-13-scope-hoisting-closure/latihan/soal10.js
@@ -0,0 +1,6 @@
+console.log(a);
+var a = 10;
+// hasinya undefined karena variabel dipamggil sebelim garisnya dibaca
+console.log(b);
+let b = 20;
+// hasilnya ReferenceError karena pakai let dan variabelnya di panggil sebelum barisnya di baca
