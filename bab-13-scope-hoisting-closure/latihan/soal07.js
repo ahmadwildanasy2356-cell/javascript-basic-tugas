bde19f92fb589931d73e6d27ae56b2b4ee1aadd5
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-11 14:23:27 +0700

    soal07

---
+++ bab-13-scope-hoisting-closure/latihan/soal07.js
@@ -0,0 +1,9 @@
+console.log(nama);
+
+var nama = "Budi";
+
+console.log(nama);
+// outputnya : undefined   budi
+// karena var itu dicatat duluan oleh komputer, tapi nilainya ditinggal di bawah.
+// di console.log pertama komputer tahu ada varibel nama tapi blm ada isinya maka hasilnya undefined
+
