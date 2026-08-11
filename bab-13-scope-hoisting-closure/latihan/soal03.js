38079c196eb50b4e0c82291f09cee0a54ed4a3a7
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-11 11:32:05 +0700

    soal03

---
+++ bab-13-scope-hoisting-closure/latihan/soal03.js
@@ -0,0 +1,10 @@
+if (true) {
+  let pesan = "Halo Santri";
+
+  console.log(pesan);
+}
+
+console.log(pesan);
+
+// outputnya : baris pertama halo santri baris kedua : error
+// error karena console.log(pesan) berada di luar kurumg kurawal karena termasuk block scope
