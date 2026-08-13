64729ce42bd5741fa9287b7d934ef63159a82582
ahmadwildanasy2356 <ahmad.wildan.asy2356@gmail.com>
2026-08-13 11:27:09 +0700

    soal09

---
+++ bab-14-date-object/latihan/soal09.js
@@ -0,0 +1,7 @@
+const tanggal = new Date(2026, 0, 1);
+
+console.log(tanggal.getMonth());
+console.log(tanggal.getDate());
+console.log(tanggal.getFullYear());
+// outputnya 0 1  2026
+// 0 karena index bulan dibaca mulai nol
